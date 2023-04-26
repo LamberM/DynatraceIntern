315f9de88ca39126f9fc4281ea51319803c4e227
LamberM <marcin.lamberski@gmail.com>
2023-04-26 21:53:09 +0200

    update

--- src/test/java/org/LamberM/UnitTest.java
+++ src/test/java/org/LamberM/UnitTest.java
@@ -1,10 +1,12 @@
 package org.LamberM;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public interface UnitTest {
 }
