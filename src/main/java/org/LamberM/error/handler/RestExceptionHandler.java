ada6aaeb4687d80a03db63cf6765cecccb25014b
LamberM <marcin.lamberski@gmail.com>
2023-04-26 21:45:45 +0200

    create new handler

--- src/main/java/org/LamberM/error/handler/RestExceptionHandler.java
+++ src/main/java/org/LamberM/error/handler/RestExceptionHandler.java
@@ -1,10 +1,11 @@
 package org.LamberM.error.handler;
 
-import org.LamberM.controllers.ProvideController;
+import org.LamberM.controllers.ExchangeRateController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
@@ -14,16 +15,21 @@ public class RestExceptionHandler {
                 .body(new RestErrorResponse(HttpStatus.BAD_REQUEST.value(), exception.getMessage()));
     }
 
-    @ExceptionHandler(value = {ProvideController.RestValidationException.class})
-    protected ResponseEntity<RestErrorResponse> handleRestValidationException(ProvideController.RestValidationException exception) {
+    @ExceptionHandler(value = {ExchangeRateController.RestValidationException.class})
+    protected ResponseEntity<RestErrorResponse> handleRestValidationException(ExchangeRateController.RestValidationException exception) {
         return ResponseEntity.badRequest()
                 .body(new RestErrorResponse(HttpStatus.BAD_REQUEST.value(), exception.getMessage()));
     }
+
 
     @ExceptionHandler(value = {Throwable.class})
     protected ResponseEntity<RestErrorResponse> handleInternalServerError(Throwable throwable) {
         return ResponseEntity.badRequest()
                 .body(new RestErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "internal error"));
+    }
+    @ExceptionHandler(value = {HttpClientErrorException.class})
+    protected ResponseEntity<RestErrorResponse> handleRestClientException(HttpClientErrorException ex) {
+        return ResponseEntity.status(ex.getStatusCode()).body(new RestErrorResponse(ex.getStatusCode().value(), ex.getMessage()));
     }
 
 }
