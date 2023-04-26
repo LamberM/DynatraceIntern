239561733779ff412329ecf993977346f087d367
LamberM <marcin.lamberski@gmail.com>
2023-04-26 21:49:22 +0200

    little update

--- src/main/java/org/LamberM/services/AverageExchangeRateService.java
+++ src/main/java/org/LamberM/services/AverageExchangeRateService.java
@@ -3,27 +3,33 @@ package org.LamberM.services;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.LamberM.utils.ExchangeRateResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Currency;
 
 @Service
 public class AverageExchangeRateService {
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
 
-    public BigDecimal getResponse(Currency currencyCode, LocalDate date) throws JsonProcessingException {
-        RestTemplate restTemplate = new RestTemplate();
-        ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    public AverageExchangeRateService(RestTemplate restTemplate, ObjectMapper objectMapper) {
+        this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
+    }
 
+    public ExchangeRateResponse calculateAverageExchangeRate(Currency currency, LocalDate date) throws JsonProcessingException {
         String formattedDate = DateTimeFormatter.ISO_LOCAL_DATE.format(date);
 
-        String url = ("https://api.nbp.pl/api/exchangerates/rates/a/" + currencyCode + "/" + formattedDate + "/?format=json");
+        String url = ("https://api.nbp.pl/api/exchangerates/rates/a/" + currency + "/" + formattedDate + "/?format=json");
         String json = restTemplate.getForObject(url, String.class);
         JsonNode rootNode = objectMapper.readTree(json);
         JsonNode midNode = rootNode.at("/rates/0/mid");
-        return midNode.decimalValue();
+        return new ExchangeRateResponse(midNode.decimalValue());
     }
 }
