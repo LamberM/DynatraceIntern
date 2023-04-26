8a49ddc5f39ae8f9855892318235eb73739f3495
LamberM <marcin.lamberski@gmail.com>
2023-04-26 21:52:55 +0200

    add bean

--- src/main/java/org/LamberM/App.java
+++ src/main/java/org/LamberM/App.java
@@ -2,10 +2,17 @@ package org.LamberM;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class App 
 {
     public static void main( String[] args ) { SpringApplication.run(App.class, args);}
 
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        return builder.build();
+    }
 }
