5a4971ddba457427686d83a584f1604ffbbd1ee3
LamberM <marcin.lamberski@gmail.com>
2023-04-26 21:48:12 +0200

    update

--- src/test/java/org/LamberM/services/MajorDifferenceBuyAskRateServiceTest.java
+++ src/test/java/org/LamberM/services/MajorDifferenceBuyAskRateServiceTest.java
@@ -10,6 +10,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Currency;
 
+
 public class MajorDifferenceBuyAskRateServiceTest implements UnitTest {
     @InjectMocks
     MajorDifferenceBuyAskRateService systemUnderTest;
@@ -18,9 +19,10 @@ public class MajorDifferenceBuyAskRateServiceTest implements UnitTest {
     void givenCodeAndTopCount_whenGetResponse_thenResponseIsCorrect() {
         //given
         Currency currency = Currency.getInstance("AUD");
+        String currencyCode = currency.toString();
         int topCount = 10;
         //when
-        BigDecimal actual = systemUnderTest.getResponse(currency, topCount).setScale(4, RoundingMode.HALF_UP);
+        BigDecimal actual = systemUnderTest.calculateMajorDifferenceBuyAskRate(currency, topCount).getAverage().setScale(4, RoundingMode.HALF_UP);
         //then
         BigDecimal expected = new BigDecimal("0.0570");
         Assertions.assertEquals(expected, actual);
